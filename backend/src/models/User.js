d61b635ffb6a67420435ed8c4292846abdb95b83
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2025-02-15 23:55:38 +0530

    user added

---
+++ backend/src/models/User.js
@@ -0,0 +1,14 @@
+// models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  resetToken: { type: DataTypes.STRING, allowNull: true },
+  resetTokenExpiry: { type: DataTypes.DATE, allowNull: true }
+}, { timestamps: true });
+
+module.exports = User;
