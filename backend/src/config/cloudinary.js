86ee270296b2d5128051fc1212482ce1d334cf2d
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2025-02-20 23:57:16 +0530

    cloudinary added

---
+++ backend/src/config/cloudinary.js
@@ -0,0 +1,7 @@
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: 'your-cloud-name',
+  api_key: 'your-api-key',
+  api_secret: 'your-api-secret'
+});
+module.exports = cloudinary;
